d47bcc69e8daba7979ca1d145ec37d77bbf4038d
chenzhichao <chenzhichao@haizhi.com>
2017-07-14 16:33:43 +0800

    修改土豆站点数据清洗函数

--- kangjia/items.py
+++ kangjia/items.py
@@ -4,22 +4,36 @@
 #
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
+import hashlib
 import logging
+
+import time
 from scrapy.item import Item, Field
-from scrapy.loader.processors import MapCompose, Identity
+from scrapy.loader.processors import MapCompose, Compose
 
 
 def extract_word(field):
     if field:
+        if isinstance(field, list):
+            field = ','.join(field)  # 补丁
         try:
-            new_field = field.split('：')[1]
+            field_list = field.split('：')
+            new_field = field_list[1]
         except:
             logging.info('该字段未知错误：{0}'.format(field))
             new_field = ''
     else:
-        new_field = ''
-    logging.info('+++++++++++++{0}++++++++++'.format(new_field))
+        new_field = field
+    # logging.info('+++++++++++++{0}++++++++++'.format(new_field))
     return new_field
+
+
+def create_record_id(name):
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    m = hashlib.md5()
+    m.update(name + current_time)
+    record_id = m.hexdigest()
+    return record_id
 
 
 class ShafaItem(Item):
@@ -53,7 +67,7 @@ class ShafaItem(Item):
 
 class TuDouItem(Item):
     program = Field()  # 节目名称
-    program_alias = Field(input_processor=MapCompose(extract_word))  # 节目别名
+    program_alias = Field(input_processor=Compose(extract_word))  # 节目别名
     area = Field()  # 地区
     image = Field()  # 封面地址
     presenter = Field()  # 主讲人
@@ -65,8 +79,11 @@ class TuDouItem(Item):
     comment_cnt = Field(input_processor=MapCompose(extract_word))  # 评论
     voteup_cnt = Field(input_processor=MapCompose(extract_word))  # 顶
     introduction = Field(input_processor=MapCompose(extract_word))  # 简介
+    applicable_age = Field(input_processor=MapCompose(extract_word))  # 适用年龄
+    first_broadcast = Field()  # 开播时间
+    director = Field()  # 编导
+    cast = Field()  # 声优
 
     _in_time = Field()
     _utime = Field()
-    _record_id = Field()
-
+    _record_id = Field(input_processor=MapCompose(create_record_id))
